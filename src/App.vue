0cbca1352ea4414373f212ce477bd66895e4d779
s.tomoki <tanajammw@gmail.com>
2020-06-29 21:12:15 +0900

    google analytics連携追加＆フッター削除

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,6 @@
     <v-content>
       <router-view></router-view>
     </v-content>
-    <v-footer
-      color="indigo"
-      app
-    >
-      <span class="white--text">&copy; 2019</span>
-    </v-footer>
   </v-app>
 </template>
 
